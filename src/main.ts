e92a25b0bfb8bc568aa5f7a2a86075189f842240
Why so serious my dear <854059946@qq.com>
2023-06-30 16:17:28 +0800

    feat: :art: mian文件测试

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: Why so serious my dear 854059946@qq.com
  * @Date: 2023-06-29 19:36:06
  * @LastEditors: Why so serious my dear 854059946@qq.com
- * @LastEditTime: 2023-06-30 15:20:09
+ * @LastEditTime: 2023-06-30 16:16:47
  * @FilePath: /my-sph-mall-admin/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,6 +16,8 @@ import pinia from '@/store'
 import registerGlobComp from './components'
 // svg插件配置
 import 'virtual:svg-icons-register'
+
+// 入口文件
 
 const app = createApp(App)
 app.use(ElementPlus)
